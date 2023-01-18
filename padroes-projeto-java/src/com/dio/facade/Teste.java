639b4abd708d34b3d6736c1599c2bc82918d579e
Mariana Benevit <marybenevit@gmail.com>
2023-01-18 19:49:59 -0300

    Finalização com Java puro

---
+++ padroes-projeto-java/src/com/dio/facade/Teste.java
@@ -0,0 +1,13 @@
+package com.dio.facade;
+
+public class Teste {
+
+    public static void main(String[] args) {
+
+        //DESIGN PATTERN FACADE
+
+        Facade facade = new Facade();
+        facade.migrarCliente("Joao", "14569001");
+
+    }
+}
