cdc1e8a9b6e9d68ae1a4a8e06c95d5f9eda9d217
Mariana Benevit <marybenevit@gmail.com>
2023-01-19 20:01:42 -0300

    Trabalhando com Spring

---
+++ padroes-projeto-spring/src/main/java/com/dio/padroesprojetospring/service/ClienteService.java
@@ -0,0 +1,17 @@
+package com.dio.padroesprojetospring.service;
+
+import com.dio.padroesprojetospring.model.Cliente;
+
+public interface ClienteService {
+
+    Iterable<Cliente> buscarTodos();
+
+    Cliente buscarPorId(Long id);
+
+    void inserir(Cliente cliente);
+
+    void atualizar(Long id, Cliente cliente);
+
+    void deletar(Long id);
+
+}
---
+++ padroes-projeto-spring/src/main/java/com/dio/padroesprojetospring/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,34 @@
+package com.dio.padroesprojetospring.service.impl;
+
+import com.dio.padroesprojetospring.model.Cliente;
+import com.dio.padroesprojetospring.service.ClienteService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ClienteServiceImpl implements ClienteService {
+
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+
+    }
+
+    @Override
+    public void deletar(Long id) {
+
+    }
+}
